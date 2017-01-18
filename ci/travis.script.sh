ae497b0bed49fc176484033aeb90a9f03188084f
Francis T. O'Donovan <francis.odonovan@gmail.com>
2017-01-18 15:43:17 -0500

    Update travis script for production

--- ci/travis.script.sh
+++ ci/travis.script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 echo "script"
+
+if [ "${TRAVIS_BRANCH}" == "production" ]; then
+    export PATH="/home/travis/.pyenv/bin:/Users/travis/.pyenv/shims:${PATH}"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    # shellcheck disable=SC1091
+    source activate venv
+fi
+
 ruby --version
 overcommit --sign
 overcommit --sign pre-commit
