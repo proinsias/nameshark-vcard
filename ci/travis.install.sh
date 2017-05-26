5ae6821e7a4a0adf1e9b47ab635f5cc518dff77f
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2017-05-26 15:21:41 -0400

    Update travis scripts

--- ci/travis.install.sh
+++ ci/travis.install.sh
@@ -23,20 +23,8 @@ ruby-lint --version
 travis version --skip-completion-check
 
 # python
-if [ "${TRAVIS_BRANCH}" == "production" ]; then
-    echo "Install pyenv"
-    export PYENV_VERSION="${PYTHON}"
-    export PATH="/home/travis/.pyenv/bin:/Users/travis/.pyenv/shims:${PATH}"
-    curl --location https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-    pyenv install "${PYTHON}"
-    pyenv virtualenv venv
-    # shellcheck disable=SC1091
-    source activate venv
-fi
 python --version
-travis_retry python -m pip install --upgrade pip wheel  # In production, use pyenv pip
+travis_retry python -m pip install --upgrade pip wheel
 travis_retry python -m pip install --require-hashes --requirement \
 ci/travis.requirements.txt
 travis_retry python -m pip install --require-hashes --requirement \
