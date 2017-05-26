50b47d2f1e6b50ab66b292937996aa3615293ad1
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2017-05-26 15:28:38 -0400

    More travis build fixes

--- ci/travis.script.sh
+++ ci/travis.script.sh
@@ -7,17 +7,9 @@ shopt -s failglob  # cause globs that don't get expanded to cause errors
 
 echo "script"
 
-if [ "${TRAVIS_BRANCH}" == "production" ]; then
-    export PATH="/home/travis/.pyenv/bin:/Users/travis/.pyenv/shims:${PATH}"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-    # shellcheck disable=SC1091
-    source activate venv
-fi
-
 ruby --version
 overcommit --sign
 overcommit --sign pre-commit
 overcommit --run
-python -m pytest  # In production, use pyenv python to launch pytest
+python -m pytest
 coverage run --source nameshark_vcard -m py.test
