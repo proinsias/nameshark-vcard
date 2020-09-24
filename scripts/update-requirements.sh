80e4925d13eb1f9eb7a3504ba9866465c85b26dc
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2020-09-24 11:21:36 -0400

    Update how we update python requirements

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -5,16 +5,12 @@ set -o nounset  # don't allow use of unset variables
 set -o pipefail  # produce a failure return code if any pipeline command errors
 shopt -s failglob  # cause globs that don't get expanded to cause errors
 
-pip-compile --verbose --no-annotate --output-file requirements.in setup.py
-rm -f requirements.txt
-touch requirements.txt
-# shellcheck disable=SC2046
-hashin --verbose $(grep -v \# requirements.in) -r requirements.txt
-rm -f requirements.in
+pip-compile --annotate --generate-hashes --header --reuse-hashes --upgrade --verbose \
+    --output-file requirements.txt setup.py
 
-pip-compile --verbose --no-annotate --output-file dev-requirements.in2 dev-requirements.in
-rm -f dev-requirements.txt
-touch dev-requirements.txt
-# shellcheck disable=SC2046
-hashin --verbose $(grep -v \# dev-requirements.in2 | cut -f 1 -d ";") -r dev-requirements.txt
-rm -f dev-requirements.in2
+safety check --file requirements.txt
+
+pip-compile --annotate --generate-hashes --header --reuse-hashes --upgrade --verbose \
+    --output-file dev-requirements.txt dev-requirements.in
+
+safety check --file dev-requirements.txt
